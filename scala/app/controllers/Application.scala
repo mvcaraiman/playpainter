85eadca893138ad2b05d5d1eb4bb349843842878
Vasile Caraiman <vca@bourse.lu>
2013-03-19 12:15:04 +0100

    Replacing Play 2.0 with 2.1 : Enumerator.imperative -> Concurrent.broadcast

--- scala/app/controllers/Application.scala
+++ scala/app/controllers/Application.scala
@@ -1,14 +1,25 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
-import play.api.libs.json._
-import play.api.libs.iteratee._
-import play.api.libs.concurrent._
-import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
+
+import scala.collection.JavaConversions.mapAsScalaDeprecatedConcurrentMap
 import scala.collection.mutable.ConcurrentMap
-import collection.JavaConversions._
+import scala.math.BigDecimal.int2bigDecimal
+import scala.math.BigDecimal.long2bigDecimal
+
+import play.api.Logger
+import play.api.libs.concurrent.Promise
+import play.api.libs.iteratee.Concurrent
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.iteratee.Iteratee
+import play.api.libs.json.JsNumber
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsString
+import play.api.libs.json.JsValue
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import play.api.mvc.WebSocket
 
 object Application extends Controller {
 
@@ -41,14 +52,12 @@ case class Painter (name: String, color: String, size: Long) {
 
 case class PaintRoom(name: String) {
   // The list of all connected painters (identified by ids)
-  val painters: ConcurrentMap[Int, Painter] = new ConcurrentHashMap[Int, Painter]()
+  import scala.collection.JavaConversions._
+  // using java.util.concurrent.ConcurrentHashMap and converting it to Scala
+  val painters: scala.collection.concurrent.Map[Int, Painter] = mapAsScalaConcurrentMap[Int,Painter](new java.util.concurrent.ConcurrentHashMap()) 
 
-  // The enum for pushing data to spread to all connected painters
-  val hubEnum = Enumerator.imperative[JsValue]()
-
-  // The hub used to get multiple output of a common input (the hubEnum)
-  val hub = Concurrent.hub[JsValue](hubEnum)
-
+  val (clientEnum, channel) = Concurrent.broadcast[JsValue]
+  
   private var counter = new AtomicInteger(0)
   private var connections = new AtomicInteger(0)
 
@@ -57,7 +66,6 @@ case class PaintRoom(name: String) {
     counter.incrementAndGet()
     connections.incrementAndGet()
     val pid = counter.get // the painter id
-
     // out: handle messages to send to the painter
     val out = 
       // Inform the painter who he is (which pid, he can them identify himself)
@@ -67,11 +75,10 @@ case class PaintRoom(name: String) {
         (painter.toJson ++ JsObject(Seq("type" -> JsString("painter"), "pid" -> JsNumber(id)))).as[JsValue]
       } toList : _*) >>> 
       // Stream the hub
-      hub.getPatchCord()
-
+      clientEnum
+      
     // in: handle messages from the painter
     val in = Iteratee.foreach[JsValue](_ match {
-
       // The painter wants to change some of his property
       case change: JsObject if (change \ "type") == JsString("change") => {
         val patchedJson = 
@@ -79,18 +86,18 @@ case class PaintRoom(name: String) {
           map(_.toJson ++ change). // if get, patch it with the change
           getOrElse(change); // otherwise, just consider the change
         painters += ((pid, Painter.fromJson(patchedJson))) // update in the map
-        hubEnum push (patchedJson ++ JsObject(Seq("pid" -> JsNumber(pid))))
+        channel push (patchedJson ++ JsObject(Seq("pid" -> JsNumber(pid))))
       }
 
       // User is drawing something
       case draw: JsObject => {
-        hubEnum push (draw ++ JsObject(Seq("pid" -> JsNumber(pid))))
+        channel push (draw ++ JsObject(Seq("pid" -> JsNumber(pid))))
       }
     }) mapDone { _ => 
       // User has disconnected.
       painters -= pid
       connections.decrementAndGet()
-      hubEnum push (JsObject(Seq("type" -> JsString("disconnect"), "pid" -> JsNumber(pid))))
+      channel push (JsObject(Seq("type" -> JsString("disconnect"), "pid" -> JsNumber(pid))))
       Logger.debug("(pid:"+pid+") disconnected.")
       Logger.info(connections+" painter(s) currently connected.");
     }
